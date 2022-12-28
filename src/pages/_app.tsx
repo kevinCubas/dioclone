abb0eacc7bcb1130992295e685ee6ef87c9e224a
unknown <61201131+kevinCubas@users.noreply.github.com>
2022-12-28 19:29:01 -0300

    set title in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,9 @@ import { GlobalStyle } from '../style/global'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>DIO Clone</title>
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,7 +26,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
        <Head>
-        <title>DIO Clone</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
